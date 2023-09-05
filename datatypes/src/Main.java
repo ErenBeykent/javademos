3de09c6fce19c61823d01293b1f2dbc39003210d
ErenBeykent <erengozsuz28@gmail.com>
2023-09-05 21:13:47 +0300

    Add project files.

---
+++ datatypes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        char karakter = 'a';
+        double sayi = 12.5;
+        sayi=-129;
+        boolean dogruMu = false;
+
+
+    }
+}
